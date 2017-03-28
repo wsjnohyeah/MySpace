be7b05e2a1b500769f0394d79d5a79a555e1b1e7
Ethan Hu <ethan.hqc@icloud.com>
2017-03-29 01:42:18 +0800

    modify about page

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -27,13 +27,22 @@
                             <li>Contact: <a href="mailto:hi@ethanhu.me">hi@ethanhu.me</a> <i class="material-icons grey-text">contact_mail</i></li>
                         </blockquote>
                         <br>
-                        <h5><i class="material-icons blue-text">build</i> Projects Working On</h5>
+                        <h5><i class="material-icons blue-text">build</i> Projects</h5>
                         <blockquote>
                             <li>Creator: School Info Sharing Site "NoticeBoard 1 &amp; 2" : <a href="http://hfinotice.com">http://hfinotice.com</a></li>
                             <br>
                             <li>Particapating in "NoticeBoard 3" : <a href="https://hfi.me">https://hfi.me</a></li>
                             <br>
-                            <li>iOS App: Texus Holdem Logger [a replacement for paper when playing this game among friends]</li>
+                            <li>iOS App: CardGames ScoreBoard [a replacement for paper scoreboards when playing card games among friends]<br>
+                                GET IT ON THE APPSTORE!<br>
+                                US: <a href="https://itunes.apple.com/us/app/cardgames-scoreboard/id1216826223?mt=8">Available in the US Store</a> <br>
+                                CHINA: <a href="https://itunes.apple.com/cn/app/cardgames-scoreboard/id1216826223?l=en&mt=8">Available in the Chinese Store</a> <br>
+                                <img src="http://ethanhu.me/postimage/Screen%20Shot%202017-03-29%20at%2001.24.14.png" style="border:1px #666 solid; width:40rem; max-width: 100%;">
+                            </li>
+                            <br>
+                            <li>iOS App: NoticeBoard for iOS. In Development. Preview: <br>
+                                <img src="http://ethanhu.me/postimage/IMG_0368.PNG" style="border:1px #666 solid;width:20rem;max-width:100%">
+                            </li>
                         </blockquote>
                     </div>
                     <br>
